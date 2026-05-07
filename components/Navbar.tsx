540a25ac7aba09bbd32694620824b28dc0bc4637
Mahyar Baghdadi <Shoppejulies@gmail.com>
2026-05-07 11:26:37 +0300

    fix logo path

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -67,7 +67,7 @@ export default function Navbar({ searchValue = "", onSearch, activePage }: Props
 
           {/* Logo */}
           <Link href="/" style={{ textDecoration: "none", flexShrink: 0 }}>
-            <Image src="/logo.png" alt="Julie's Shoppe" width={100} height={26}
+            <Image src="/logo.jpg" alt="Julie's Shoppe" width={100} height={26}
               style={{ filter: "invert(1) brightness(0.08)", objectFit: "contain" }} priority />
           </Link>
 
